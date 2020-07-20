d12ae45e9dc4b7bdfcb0931fbc1ab73d9c9d5749
Timothy Lalli <trlalli92@gmail.com>
2020-07-20 03:13:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name 
+  end   
+
+  def match(numbers)
+    numbers.select do |num| 
+      num.split("").sort == @name.split("").sort
+    end
+  end
+end 
